79dd4920691a087b7009563860c5be29b199bf90
rananagash <rana.nagash@mail.utoronto.ca>
2025-03-19 16:10:59 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -13,18 +13,18 @@ class Graph:
     #     - _vertices:
     #         A collection of the vertices contained in this graph.
     #         Maps item to _Vertex object.
-    _vertices: dict[Any, Vertex]
+    vertices: dict[Any, Vertex]
 
     def __init__(self) -> None:
         """Initialize an empty graph (no vertices or edges)."""
-        self._vertices = {}
+        self.vertices = {}
 
     def add_vertex(self, item: Any) -> None:
         """Add a vertex with the given item to this graph.
 
         The new vertex is not adjacent to any other vertices.
         """
-        self._vertices[item] = Vertex(item)
+        self.vertices[item] = Vertex(item)
 
     def add_edge(self, item1: Any, item2: Any, weight: int) -> None:
         """Add an edge between the two vertices with the given items in this graph.
@@ -34,9 +34,9 @@ class Graph:
         Preconditions:
             - item1 != item2
         """
-        if item1 in self._vertices and item2 in self._vertices:
-            v1 = self._vertices[item1]
-            v2 = self._vertices[item2]
+        if item1 in self.vertices and item2 in self.vertices:
+            v1 = self.vertices[item1]
+            v2 = self.vertices[item2]
 
             # Add the new edge
             v1.add_neighbour(v2, weight)
@@ -52,8 +52,8 @@ class Graph:
 
         Raise a ValueError if item does not appear as a vertex in this graph.
         """
-        if item in self._vertices:
-            v = self._vertices[item]
+        if item in self.vertices:
+            v = self.vertices[item]
             return {neighbour.item for neighbour in v.neighbours}
         else:
             raise ValueError
@@ -63,8 +63,8 @@ class Graph:
 
         Return False if item1 or item2 do not appear as vertices in this graph.
         """
-        if item1 in self._vertices and item2 in self._vertices:
-            v1 = self._vertices[item1]
+        if item1 in self.vertices and item2 in self.vertices:
+            v1 = self.vertices[item1]
             return any(v2.item == item2 for v2 in v1.neighbours)
         else:
             # We didn't find an existing vertex for both items.
